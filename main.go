c2598352de64e3e21fc0ce960936d33d1eaf76b0
willian souza <willianolsouza@hotmail.com>
2026-06-22 23:52:01 -0300

    feat: criado request e response

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Blackwillsz/crud---go.git/src/controller/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-    fmt.Println(os.Getenv("TEST")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/Blackwillsz/crud---go.git/src/configuration/rest_err"
+	"github.com/Blackwillsz/crud---go.git/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
 
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
